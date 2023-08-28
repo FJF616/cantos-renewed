8b161508790e58a698cbfd22481cec9ea64855e0
FJF616 <franzjfernando@hotmail.com>
2023-08-28 16:04:16 -0700

    Update CantosSpec.cy.js
    initial commit

--- cypress/component/CantosSpec.cy.js
+++ cypress/component/CantosSpec.cy.js
@@ -1,6 +1,6 @@
 import CantosText from "../../src/CantosText";
-describe("CantosSpec.cy.js", () => {
-  it("generates a randomized list of canto entries", () => {
+describe("<CantosText>", () => {
+  beforeEach(() => {
     cy.mount(<CantosText />);
   });
 });
